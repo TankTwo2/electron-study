0c297b0922b29082fc1222b346d7faf3c3d13370
Tanktwo2 <rhkdtjr9@naver.com>
2022-10-16 11:10:02 +0900

    M2

--- preload.ts
+++ preload.ts
@@ -1,6 +1,8 @@
+// DOMContentLoaded 이벤트는 초기 HTML 문서를 완전히 불러오고 분석했을 때 발생합니다.
+// 스타일 시트, 이미지, 하위 프레임의 로딩은 기다리지 않습니다.
 
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
+  const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
   }
